4e44426134894527ba49553f57dbeed0b711fa58
messat <148489126+messat@users.noreply.github.com>
2025-01-21 20:13:24 +0000

    This commit adds new user schema to the database. Added new route to register user using POST Route. Utilised bcrypt package to hash and salt the password before it is stored in the database. Completed comprenshive testing to handle Mongoserver errors and Validation errors(mongoose).

--- errorHandlers/errorHandlers.js
+++ errorHandlers/errorHandlers.js
@@ -1,20 +1,34 @@
 exports.handleMongoErrors = (err, req, res, next) => {
-       if(err.reason){
-            res.status(400).send({msg: "400 Bad Request"})
-        }
-        next(err)   
-}
-
-exports.validationErrors = (err, req, res, next) => {
-    if(err.errors.title || err.errors.date || err.errors.description || err.errors.location || err.errors.event_img_url || err.errors.price || err.errors.duration || err.errors.category || err.errors.spaces) {
-        res.status(400).send({msg: "400 Missing Validation"})
+    if(err.name === "Error"){
+        res.status(400).send({msg: "400 Bad Request", error: err})
     }
-    next(err)
+    if(err.name === "MongoServerError" && err.errorResponse.code === 11000) {
+        res.status(401).send({msg: "401 User already exists"})
+    } else if(err.reason){
+        res.status(400).send({msg: "400 Bad Request"})
+    } 
+    else {
+        next(err)   
+    }
+    }
+    
+exports.validationErrors = (err, req, res, next) => {
+    if(err.name === "ValidationError"){
+        const validationError = Object.keys(err.errors).map((field) => err.errors[field].message)
+        res.status(400).send({msg: "400 Validation Error", errors: validationError})
+    } else {
+        next(err)
+    }
 }
 
 exports.customErrors = (err, req, res, next) => {
     if(err.status === 404 && err.msg === "404 Route Not Found"){
         res.status(err.status).send(err.msg)
+    } else {
+        next(err)
     }
-    next(err)
+}
+
+exports.serverError = (err, req, res, next) => {
+    res.status(500).send({msg: "Internal Server side error"})
 }
